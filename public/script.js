31e9340f1fe57416ff6f4ac8cdeb559fa02690de
Emma McLuckie <emmamcluckie@outlook.com>
2023-06-02 17:36:15 +1000

    fighting with my stupid form

--- public/script.js
+++ public/script.js
@@ -11,6 +11,9 @@ const booklist = document.getElementById("bookList");
 form.addEventListener("submit", function (event) {
     event.preventDefault();
     const tagsArray = tagify.value.map((tag) => tag.value); // Extract the tag values
+    
+// (cover, title, author, genre, format, length, startDate, rating, review, tags)
+
     addBook(
         form.elements.bookCover.value,
         form.elements.bookTitle.value,
@@ -18,7 +21,8 @@ form.addEventListener("submit", function (event) {
         form.elements.bookGenre.value,
         form.elements.bookFormat.value,
         form.elements.bookLength.value,
-        form.elements.bookRating.value,
+        form.elements.bookStartDate.value,
+        document.querySelector('input[name="bookRating"]:checked').value,
         form.elements.bookReview.value,
         tagsArray
     );
@@ -145,3 +149,102 @@ function addBook(cover, title, author, genre, format, length, startDate, rating,
 
 // Call the function with test values for the input paramaters
 // addBook();
+
+
+
+
+// javascript for multi step form page on canvas - 
+// "Code taken from https://webdesign.tutsplus.com/tutorials/how-to-build-a-multi-step-form-wizard-with-javascript--cms-93342"
+
+const previousButton = document.querySelector('#prev')
+const nextButton = document.querySelector('#next')
+const submitButton = document.querySelector('#submit')
+const tabTargets = document.querySelectorAll('.tab')
+const tabPanels = document.querySelectorAll('.tabpanel')
+const isEmpty = (str) => !str.trim().length
+let currentStep = 0
+
+// Validate first input on load
+validateEntry()
+
+// Next: Change UI relative to current step and account for button permissions
+nextButton.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  // Hide current tab
+  tabPanels[currentStep].classList.add('hidden')
+  tabTargets[currentStep].classList.remove('active')
+
+  // Show next tab
+  tabPanels[currentStep + 1].classList.remove('hidden')
+  tabTargets[currentStep + 1].classList.add('active')
+  currentStep += 1
+  
+  validateEntry()
+  updateStatusDisplay()
+})
+
+// Previous: Change UI relative to current step and account for button permissions
+previousButton.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  // Hide current tab
+  tabPanels[currentStep].classList.add('hidden')
+  tabTargets[currentStep].classList.remove('active')
+
+  // Show previous tab
+  tabPanels[currentStep - 1].classList.remove('hidden')
+  tabTargets[currentStep - 1].classList.add('active')
+  currentStep -= 1
+
+  nextButton.removeAttribute('disabled')
+  updateStatusDisplay()
+})
+
+
+function updateStatusDisplay() {
+  // If on the last step, hide the next button and show submit
+  if (currentStep === tabTargets.length - 1) {
+    nextButton.classList.add('hidden')
+    previousButton.classList.remove('hidden')
+    submitButton.classList.remove('hidden')
+    validateEntry()
+
+    // If it's the first step hide the previous button
+  } else if (currentStep == 0) {
+    nextButton.classList.remove('hidden')
+    previousButton.classList.add('hidden')
+    submitButton.classList.add('hidden')
+    // In all other instances display both buttons
+  } else {
+    nextButton.classList.remove('hidden')
+    previousButton.classList.remove('hidden')
+    submitButton.classList.add('hidden')
+  }
+}
+
+function validateEntry() {
+  let input = tabPanels[currentStep].querySelector('.form-input')
+  
+  // Start but disabling continue button
+  nextButton.setAttribute('disabled', true)
+  submitButton.setAttribute('disabled', true)
+  
+  // Validate on initial function fire
+  setButtonPermissions(input)
+  
+  // Validate on input
+  input.addEventListener('input', () => setButtonPermissions(input))
+  // Validate if bluring from input
+  input.addEventListener('blur', () => setButtonPermissions(input))
+}
+
+function setButtonPermissions(input) {
+  if (isEmpty(input.value)) {
+    nextButton.setAttribute('disabled', true)
+    submitButton.setAttribute('disabled', true)
+  } else {
+    nextButton.removeAttribute('disabled')
+    submitButton.removeAttribute('disabled')
+  }
+}
